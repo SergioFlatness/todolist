3771dea4acf9f85003ff7f624ef4e28eb398f8e9
sergio <117276020+SergioFlatness@users.noreply.github.com>
2024-07-09 19:02:35 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+let taskInput=document.getElementById("task-input");
+let addbutton=document.getElementById("add-button");
+let taskList=[]
+addbutton.addEventListener("click",addTask)
+
+function addTask(){
+    let taskContent=taskInput.value
+    taskList.push(taskContent);
+    console.log(taskList);
+    render();
+}
+
+function render(){
+    let resultHTML="";
+    for(let i=0;i<taskList.length;i++){
+        resultHTML+=`<div class="task">
+            <div>${taskList[i]}</div>
+            <div>
+                <button>Check</button>
+                <button>delete</button>
+
+            </div>
+        </div>`;
+    }
+
+
+    document.getElementById("task-board").innerHTML=resultHTML;
+}
