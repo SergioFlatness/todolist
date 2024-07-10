b155cc65e3041bc41b3aa76c58823cb500c05cf9
sergio <117276020+SergioFlatness@users.noreply.github.com>
2024-07-10 21:05:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,69 @@
-let taskInput=document.getElementById("task-input");
-let addbutton=document.getElementById("add-button");
-let taskList=[]
-addbutton.addEventListener("click",addTask)
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let taskList = [];
+addButton.addEventListener("click",addTask);
 
 function addTask(){
-    let taskContent=taskInput.value
-    taskList.push(taskContent);
+    let task = {
+        id:randomIDGenerate(),         
+        taskContent: taskInput.value,
+        isComplete: false,        
+    };  
+    taskList.push(task);  
     console.log(taskList);
     render();
 }
 
-function render(){
-    let resultHTML="";
-    for(let i=0;i<taskList.length;i++){
-        resultHTML+=`<div class="task">
-            <div>${taskList[i]}</div>
+function render(){  
+    let resultHTML = "";
+    for(let i = 0 ; i < taskList.length; i++){
+        if(taskList[i].isComplete == true){
+            resultHTML += `<div class="task">
+                    <div class = "task-done">${taskList[i].taskContent}</div>
+                    <div>
+                        <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                        <button onclick="DeleteTask('${taskList[i].id}')">Delete</button>
+                    </div>
+                </div>`
+        }else{
+            resultHTML += `<div class="task">
+            <div>${taskList[i].taskContent}</div>
             <div>
-                <button>Check</button>
-                <button>delete</button>
-
+                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button onclick="DeleteTask('${taskList[i].id}')">Delete</button>
             </div>
         </div>`;
+        }
+
+     
     }
 
+    document.getElementById("task-board").innerHTML = resultHTML
 
-    document.getElementById("task-board").innerHTML=resultHTML;
+}
+
+function toggleComplete(id){
+    console.log("id:",id);
+    for(let i = 0; i< taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList[i].isComplete = !taskList[i].isComplete;  
+            break;
+        }
+    }
+    render();  
+    console.log(taskList)
+}
+
+function DeleteTask(id){ 
+    for(let i = 0; i < taskList.length ; i++){
+        if(taskList[i].id == id){
+            taskList.splice(i,1);
+            break;
+        }
+    } 
+    render();  
+}
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
